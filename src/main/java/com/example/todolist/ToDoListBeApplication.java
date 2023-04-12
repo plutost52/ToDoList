ea7fa1a3d18e331ae8b960e28416985907319e7f
syckor <syckor@naver.com>
2023-04-12 22:48:08 +0900

    프로젝트 초기 세팅

--- src/main/java/com/example/todolist/ToDoListBeApplication.java
+++ src/main/java/com/example/todolist/ToDoListBeApplication.java
@@ -1,4 +1,4 @@
-package com.example.todolistbe;
+package com.example.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
