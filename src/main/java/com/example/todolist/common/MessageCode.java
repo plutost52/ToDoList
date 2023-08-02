c00f132c1984ee783e9c16aa6a6a37f129d1844e
syckor <syckor@naver.com>
2023-08-02 20:50:02 +0900

    로그아웃 처리 - 로그아웃 이력 저장만 함

--- src/main/java/com/example/todolist/common/MessageCode.java
+++ src/main/java/com/example/todolist/common/MessageCode.java
@@ -13,6 +13,7 @@ public enum MessageCode {
     CHECK_EMAIL("유효한 이메일입니다.", 200),
     SUCCESS_SIGN_UP("회원가입 성공", 200),
     SUCCESS_MEMBER_UPDATE("회원 정보 수정 성공", 200),
+    SUCCESS_MEMBER_LOGOUT("로그아웃 처리 성공", 200),
 
     CARD_CREATE_SUCCESS("카드 추가 성공", 200),
     CARD_DELETE_SUCCESS("카드 삭제 성공", 200),
