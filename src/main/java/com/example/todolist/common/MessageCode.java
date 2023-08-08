8b1cbd6ff30e3fdb4e661de63300ad1a0f1425be
syckor <syckor@naver.com>
2023-08-08 22:32:07 +0900

    카드에 카드라인 등록

--- src/main/java/com/example/todolist/common/MessageCode.java
+++ src/main/java/com/example/todolist/common/MessageCode.java
@@ -20,7 +20,8 @@ public enum MessageCode {
     CARD_READ_SUCCESS("카드 목록 조회 성공", 200),
     CARD_LIST_SUCCESS("카드 목록 조회 성공", 200),
     CARD_UPDATETITLE_SUCCESS("카드 제목 저장 성공", 200),
-    CARD_UPDATEDONE_SUCCESS("카드 상태변경 저장 성공", 200);
+    CARD_UPDATEDONE_SUCCESS("카드 상태변경 저장 성공", 200),
+    CARDLINE_CREATE_SUCCESS("카드라인 추가 성공", 200);
 
     private final String msg;
     private final int statusCode;
